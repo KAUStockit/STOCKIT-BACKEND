8c109191aa915f74b70b31a7c4c6e3a9860b5fbc
thkthk97@naver.com <thkthk97@naver.com>
2021-05-16 01:27:47 +0900

    Initial Commit

---
+++ src/main/java/Stockit/StockitApplication.java
@@ -0,0 +1,13 @@
+package Stockit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockitApplication.class, args);
+	}
+
+}
