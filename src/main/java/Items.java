789bcbb29118dc0e37e34b618ba74925187a3908
choi <enlqn1010@gmail.com>
2021-05-16 23:33:43 +0900

    testing0.02

---
+++ src/main/java/Items.java
@@ -0,0 +1,9 @@
+
+public class Items {
+    // code -> varchar(45), name(company) -> varchar(45), is_active -> boolean, issuance->double, url->varchar(45)
+    private String code;
+    private String name;
+    private boolean is_active;
+    private double issurance;
+    private String url;
+}
