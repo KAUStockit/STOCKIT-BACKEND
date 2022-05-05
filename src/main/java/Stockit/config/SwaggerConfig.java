8bf6c7d5b3311ec89f01bc91d172adf8073a8f7e
taehyungz <thkthk97@naver.com>
2022-05-05 20:17:33 +0900

    config: swagger 설정 추가

---
+++ src/main/java/Stockit/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package Stockit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    private static final String API_NAME = "STOCKIT API";
+    private static final String API_VERSION = "0.0.1";
+    private static final String API_DESCRIPTION = "STOCKIT API 명세서";
+
+    @Bean
+    public Docket swagger() {
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
